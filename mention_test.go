c59f20694f2f9723b6bec4b7687e04b264e0b241
Vojtech Vitek (V-Teq) <vojtech.vitek@pressly.com>
2015-10-13 13:29:59 -0400

    Add more comprehensive tests for mentions

--- mention_test.go
+++ mention_test.go
@@ -18,6 +18,8 @@ func TestGetTag(t *testing.T) {
 		{" @gernest @mwanza", []string{"gernest", "mwanza"}},
 		{" @gernest @mwanza ", []string{"gernest", "mwanza"}},
 		{" @gernest @mwanza @tanzania", []string{"gernest", "mwanza", "tanzania"}},
+		{"@valid10 @valid-dash @valid-dash10", []string{"valid10", "valid-dash", "valid-dash10"}},
+		{"email@example.com @10 @10x @@x @invalid--doubledash @invalid- @-invalid", []string{}},
 	}
 	for _, v := range sample {
 		tag := GetTags('@', strings.NewReader(v.src))
